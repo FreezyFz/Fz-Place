4b332d70714a627f54d85026d74172c45fc7e47b
FreezyFz <b12565b@Yahoo.com.tw>
2017-04-06 23:04:30 +0800

    new
    new

--- Hw5/app.js
+++ Hw5/app.js
@@ -6,7 +6,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
 var port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 app.use('/pic', express.static(__dirname + '/pic'));
 app.use('/css', express.static(__dirname + '/css'));
@@ -16,12 +16,24 @@ app.use('/', function (req, res, next) {
 	next();
 });
 
-app.get('/', function(req, res) {
-	res.render('file1');
+app.get('/mine', function(req, res) {
+	res.render('mine');
 });
 
 app.get('/page2', function(req, res) {
-	res.render('file2');
+	res.render('page2');
+});
+
+app.get('/page3', function(req, res) {
+	res.render('page3');
+});
+
+app.get('/page4', function(req, res) {
+	res.render('page4');
+});
+
+app.get('/page5', function(req, res) {
+	res.render('page5');
 });
 
 app.post('/form', urlencodedParser, function(req,res){
